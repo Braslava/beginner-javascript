7dcf7ce077f8812b0b0eeccd055a839f182af86b
Your Name <m.braslava@gmail.com>
2021-12-18 20:26:04 +0200

    scrolling events practice

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,3 +1,18 @@
-console.log('it works!');
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+const acceptButton = document.querySelector('.accept');
 
-const terms = document.querySelector('.temrs-and-conditions');
+function obCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    acceptButton.disabled = false;
+    // stop observing the button
+    ob.unobserve(terms.lastElementChild);
+  }
+}
+
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 1,
+});
+
+ob.observe(terms.lastElementChild);
