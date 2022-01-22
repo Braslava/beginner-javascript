8e9afa841b4754d4b0e83f0beaab4435e09f8cfd
Your Name <m.braslava@gmail.com>
2022-01-22 23:48:15 +0200

    dad jokes exercise

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -1,3 +1,7 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
+
 const buttonText = [
   'Ugh.',
   '🤦🏻‍♂️',
@@ -8,3 +12,40 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+async function fetchJoke() {
+  // turn on loader
+  loader.classList.remove('hidden');
+  jokeButton.classList.add('hidden');
+  const response = await fetch(`https://icanhazdadjoke.com/`, {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const data = await response.json();
+  // turn the loader off
+  loader.classList.add('hidden');
+  jokeButton.classList.remove('hidden');
+  return data;
+}
+
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  if (item === not) {
+    console.log('We used that one last time!');
+    return randomItemFromArray(arr, not);
+  }
+  return item;
+}
+
+async function handleClick() {
+  // destructuring the returned data object because it has a joke property
+  const { joke } = await fetchJoke();
+  jokeHolder.textContent = joke;
+  jokeButton.textContent = randomItemFromArray(
+    buttonText,
+    jokeButton.textContent
+  );
+}
+
+jokeButton.addEventListener('click', handleClick);
